bf9b21b69772a0b10f5df9c7855fc3873b7e6825
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-01 23:04:57 +0530

    Time: 17 ms (5.92%) | Memory: 8.5 MB (59.21%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+
+
     int hIndex(vector<int>& citations) {
         
         int n=citations.size();
-
-        int minv=INT_MAX, maxv = INT_MIN;
-
-        for(int i=0; i<n; i++){
-            maxv = max(maxv,citations[i]);
-            minv = min(minv, citations[i]);
-        }
-        unordered_map<int,int>hidx;
-
-        for(int i=0; i<=maxv; i++){
-            int cnt=0;
-            for(int j=0; j<n; j++){
-                if(citations[j]>=i) cnt++;
-            }
-            hidx[i]=cnt;
-        }
+        int maxe;
+        sort(citations.begin(), citations.end());
+        maxe = citations[n-1];
 
         int ans=-1;
-        for(auto it:hidx){
-            if(it.second>=it.first){
-                ans = max(ans, it.first);
+        for(int i=0; i<=maxe; i++){
+            int papers=0;
+            for(int j=0; j<n; j++){
+                if(citations[j]>=i){
+                    papers++;
+                }
+            }
+            if(papers>=i){
+                ans = max(ans, i);
+            }
+            else{
+                break;
             }
         }
 
         return ans;
-
     }
 };
