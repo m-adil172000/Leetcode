82e8485061937a5aea9ca1d9a6f7936fdb33573f
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-03 22:06:53 +0530

    Time: 4 ms (93.08%) | Memory: 8 MB (90.86%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -6,6 +6,9 @@ public:
         int n=s.length();
         string ans="";
 
+        // First and last row each have jumps of 2*(numRows-1) between the characters of string i.e. P...A...H...N
+        // the rows in between have have more character between each column and they are at a jump of (i+inc - 2*r)
+
         for(int r=0; r<numRows; r++){
             int inc = 2*(numRows-1);
             for(int i=r; i<n ; i+=inc){
