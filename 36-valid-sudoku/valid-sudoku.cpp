36ba22c15e323c1ab294609db3c34a62bf3f5d5c
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-04 15:22:16 +0530

    Time: 17 ms (73.50%) | Memory: 18.1 MB (70.77%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,154 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0; i<9; i++){
-            unordered_map<char,int>mp;
-            for(int j=0; j<9; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-            mp.clear();
-        }
-        for(int i=0; i<9; i++){
-            unordered_map<char,int>mp;
-            for(int j=0; j<9; j++){
-                if(board[j][i]!='.'){
-                    if(mp[board[j][i]]==0){
-                        mp[board[j][i]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-            mp.clear();
-        }
+        int row[9][9]={0};
+        int col[9][9]={0};
+        int grid[9][9]={0};
 
-        unordered_map<char,int>mp;
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+
+                    int num = board[i][j]-'1';
+                    int gridnum = (i/3)*3 + (j/3);
+
+                    if(row[i][num] || col[j][num] || grid[gridnum][num]){
                         return false;
                     }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=0; i<3; i++){
-            for(int j=3; j<6; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=0; i<3; i++){
-            for(int j=6; j<9; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=3; i<6; i++){
-            for(int j=0; j<3; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=3; i<6; i++){
-            for(int j=3; j<6; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=3; i<6; i++){
-            for(int j=6; j<9; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6; i<9; i++){
-            for(int j=0; j<3; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6; i<9; i++){
-            for(int j=3; j<6; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6; i<9; i++){
-            for(int j=6; j<9; j++){
-                if(board[i][j]!='.'){
-                    if(mp[board[i][j]]==0){
-                        mp[board[i][j]]=1;
-                    }
-                    else{
-                        return false;
-                    }
+                    row[i][num] = col[j][num] = grid[gridnum][num] =1;
                 }
             }
         }
 
         return true;
-
     }
 };
