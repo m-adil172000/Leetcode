80f68bb14d4c01328075a752773dae240a0bf1f7
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-05 18:57:27 +0530

    Time: 89 ms (98.80%) | Memory: 10.4 MB (30.51%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
-    vector<bool> sieve(int n){
+    int countPrimes(int n) {
+        vector<bool> v(n,true);
+        if(v.size()<3) return 0; // because we have to check upto less than n
+
+        v[0]=v[1]=false;
+        int count=1;
+
+        for(int i=3; i<n; i+=2){//not checking even numbers bcz they are non-primes
+            if(v[i]){
+                count++;
+                //only updating odd multiples of i bcz even multiples are non-prime
+                for(int j=i*3 ; j<n; j+=i+i){
+                    v[j]=false;
+                }
+            }
+        }
+
+        return count;
+    }
+    
+};
+
+/*
+vector<bool> sieve(int n){
         vector<bool>isprime(n+1,true);
         isprime[0]=isprime[1]=false;
-        for(int i=2; i*i<=n; i++){
+        for(int i=2; i*i<=n; i+=2){ 
             int j=2;
             while(i*j<=n){
                 isprime[i*j]=false;
@@ -23,4 +46,8 @@ public:
 
         return ans;
     }
-};
+    // Time Complexity : O(n*log(log(n))
+    // Space Complexity : O(n)
+
+
+*/
