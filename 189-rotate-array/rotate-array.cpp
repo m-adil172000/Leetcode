6f33b114c3cb5e9f866e184c1525ea4f8fc22631
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-06 13:52:09 +0530

    Time: 23 ms (69.22%) | Memory: 24.9 MB (90.66%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        /*
+        Example:-1234567 ,k=3
+        1.first reverse the numbers form index 0 to n-k;
+        ->4321 567
+        2.reverse the k elements from the last
+        ->4321 765
+        3.now reverse the whole nums;
+        ->5671234 Done Answer is here !!!!
+        */ 
         k = k%(nums.size());
         int n =nums.size();
         
