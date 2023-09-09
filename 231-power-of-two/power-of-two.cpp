1b25c51638737141fef775139830374e5a87bb06
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-09 23:37:52 +0530

    Time: 5 ms (15.39%) | Memory: 6 MB (49.51%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int cnt=0;
+        // if a number is a power of 2 then it will have only one set bit
+        // and the number less than that number will have all other set bits before the set bit in the above number
+        // for eg: 8(1000) , 7(0111)
+        //  16(10000) , 15(01111)
+
+        if(n<=0) return false;
+
+        return (n&(n-1))==0;
+    }
+};
+
+/*
+int cnt=0;
         while(n>0){
             n = n&(n-1);
             cnt++;
         }
 
         return cnt==1;
-    }
-};
+*/
