22d5d0f5f29b4e35e812dc3d4a7b1ff5ce6d2190
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-09-21 18:44:50 +0530

    Time: 3 ms (83.74%) | Memory: 8.2 MB (44.60%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        vector<int>dp(n+1,0);
+        int offset=1;
+
+        for(int i=1; i<=n; i++){
+            if(offset*2 == i){
+                offset = i;
+            }
+
+            dp[i] = 1 + dp[i-offset];
+        }
+
+        return dp;
+    }
+};
+
+
+/*
+this is also O(n) solution , by me
+
+vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
         ans[0]=0;
         if(n==0){
@@ -23,4 +43,5 @@ public:
 
         return ans;
     }
-};
+
+*/
