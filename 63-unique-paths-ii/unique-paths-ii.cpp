293e8d8324992e3abf6b37f41c6071941f9dd28a
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-04 11:03:28 +0530

    Time: 2 ms (61.53%) | Memory: 8 MB (15.39%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,49 +1,35 @@
 class Solution {
-private:
-    int grid(vector<vector<int>>& obstacleGrid,int i, int j, vector<vector<int>>&dp){
-
-        if(i==0 && j==0 && obstacleGrid[i][j]!=1){
-            return 1;
-        }
-        if(i<0 || j<0 || obstacleGrid[i][j]==1){
-            return 0;
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int left = grid(obstacleGrid, i, j-1, dp);
-        int up = grid(obstacleGrid, i-1, j, dp);
-
-        return dp[i][j] = left+up;
-    }
-
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        vector<vector<int>>dp(n, vector<int>(m,0));
-        if(obstacleGrid[0][0]==1){
-            dp[0][0]=0;
-        }
-        else dp[0][0]=1;
+        vector<int>prev(m,0);
 
         for(int i=0; i<n;i++){
+            vector<int>curr(m,0);
             for(int j=0; j<m; j++){
 
-                if(i==0 && j==0) continue;
+                if(i==0 && j==0){
+                    if(obstacleGrid[0][0]==1){
+                        curr[0]=0;
+                    }
+                    else curr[0]=1;
+                    continue;
+                }
 
-                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                if(obstacleGrid[i][j]==1) curr[j]=0;
                 else{
                     int left = 0, up=0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
 
-                    dp[i][j] = left + up;
+                    curr[j] = left + up;
                 }
             }
+            prev=curr;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
