bc7a3709348df9b7d756ca7c814b36e41f73b0aa
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-04 10:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (8.25%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    long long grid(int i, int j ,vector<vector<int>>&dp){
+
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        long long up = grid(i-1,j,dp);
+        long left = grid(i,j-1,dp);
+
+        return dp[i][j] = up+left;
+
+    }    
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        dp[0][0]=1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int up=0, left=0;
+                if(i==0 && j==0) continue;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
+
+    }
+};
