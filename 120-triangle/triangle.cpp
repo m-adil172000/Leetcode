8c4bf8e863fa9f3b6db632cfaf1067d9dc72d9a2
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-04 19:19:13 +0530

    Time: 4 ms (75.21%) | Memory: 9.3 MB (18.25%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,38 +1,26 @@
 class Solution {
-private:
-    int path(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp){
-        if(i==n-1){
-            return triangle[n-1][j];
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        long long down=INT_MAX, diagonal = INT_MAX;
-
-        down = triangle[i][j]+path(i+1,j,n,triangle,dp);
-        diagonal = triangle[i][j] + path(i+1,j+1,n,triangle,dp);
-
-        return dp[i][j] = min(down,diagonal);
-    }
 
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(triangle[n-1].size(),0));
+        vector<int>prev(triangle[n-1].size(),0);
 
-        for(int i=0; i<triangle[n-1].size(); i++) dp[n-1][i] = triangle[n-1][i];
+        for(int i=0; i<triangle[n-1].size(); i++) prev[i] = triangle[n-1][i];
 
         for(int i=n-2; i>=0; i--){
+            vector<int>curr(i+1,0);
             for(int j=i; j>=0; j--){
                 long long down=INT_MAX, diagonal = INT_MAX;
 
-                down = triangle[i][j]+dp[i+1][j];
-                diagonal = triangle[i][j] + dp[i+1][j+1];
+                down = triangle[i][j]+prev[j];
+                diagonal = triangle[i][j] + prev[j+1];
 
-                dp[i][j] = min(down,diagonal);
+                curr[j] = min(down,diagonal);
             }
+            prev= curr;
+
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 };
