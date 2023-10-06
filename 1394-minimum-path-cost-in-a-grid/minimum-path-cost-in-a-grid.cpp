aee574adcab166ab6dcbdb9b409cf72d7c908df2
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-06 15:49:18 +0530

    Time: 242 ms (83.90%) | Memory: 80.7 MB (27.12%) - LeetSync

--- 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -1,28 +1,28 @@
 class Solution {
-private:
-    int solve(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>& moveCost, vector<vector<int>>&dp){
-        if(i==n-1){
-            return grid[i][j];
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int ans=INT_MAX;
-        for(int k=0; k<m; k++){
-            int curr = grid[i][j] + moveCost[grid[i][j]][k] + solve(i+1,k,n,m,grid,moveCost,dp);
-            ans = min(curr,ans);
-        }
-
-        return dp[i][j] = ans;
-    }
 public:
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         int n = grid.size();
         int m = grid[0].size();
         // we can move from any valid cell of ith row to any other valid cell of (i+1)th row
+        vector<vector<int>>dp(n,vector<int>(m,0));
+
+        for(int j=0; j<m; j++) dp[n-1][j]=grid[n-1][j];
+
+        for(int i = n-2; i>=0; i--){
+            for(int j=0; j<m; j++){
+                int temp=INT_MAX;
+                for(int k=0; k<m; k++){
+                    temp = min(temp,grid[i][j] + moveCost[grid[i][j]][k] + dp[i+1][k]);
+                }
+                dp[i][j]=temp;
+                
+            }
+        }
+
+       
         int ans=INT_MAX;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        for(int j=0; j<m; j++){
-            ans = min(ans,solve(0,j,n,m,grid,moveCost,dp));
+        for(int i=0; i<m; i++){
+            ans= min(ans,dp[0][i]);
         }
 
         return ans;
