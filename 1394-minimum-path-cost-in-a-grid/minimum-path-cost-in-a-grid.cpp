7c8f9ec69f40e5ec7e72b12694bfa936ad0f57f1
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-06 15:51:32 +0530

    Time: 264 ms (40.40%) | Memory: 80.4 MB (75.05%) - LeetSync

--- 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -4,25 +4,28 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         // we can move from any valid cell of ith row to any other valid cell of (i+1)th row
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0), curr(m,0);
 
-        for(int j=0; j<m; j++) dp[n-1][j]=grid[n-1][j];
+        for(int j=0; j<m; j++){
+            prev[j]=grid[n-1][j];
+            curr[j]=grid[n-1][j];
+        } 
 
         for(int i = n-2; i>=0; i--){
             for(int j=0; j<m; j++){
                 int temp=INT_MAX;
                 for(int k=0; k<m; k++){
-                    temp = min(temp,grid[i][j] + moveCost[grid[i][j]][k] + dp[i+1][k]);
+                    temp = min(temp,grid[i][j] + moveCost[grid[i][j]][k] + prev[k]);
                 }
-                dp[i][j]=temp;
-                
+                curr[j]=temp;
             }
+            prev=curr;
         }
 
        
         int ans=INT_MAX;
         for(int i=0; i<m; i++){
-            ans= min(ans,dp[0][i]);
+            ans= min(ans,prev[i]);
         }
 
         return ans;
