b95e692c6366056b4f682505ef005e53f1c220a3
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-07 00:02:23 +0530

    Time: 14 ms (24.94%) | Memory: 13.7 MB (28.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int,bool>row;
-        unordered_map<int,bool>col;
-
-        int n=matrix.size();
-        int m=matrix[0].size();
-
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool flag1 = false, flag2 = false;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    row[i]=true;
-                    col[j]=true;
-                }
+            if(matrix[i][0] == 0){
+                flag1 = true;
             }
         }
-
-        for(int i=0; i<n; i++){
-            if(row[i]){
-                for(int j=0; j<m; j++) matrix[i][j]=0;
-            }
-        }
-
         for(int j=0; j<m; j++){
-            if(col[j]){
-                for(int i=0; i<n; i++){
-                    matrix[i][j]=0;
+            if(matrix[0][j] == 0){
+                flag2 = true;
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
-
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(flag1 == true){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(flag2 == true){
+            for(int j=0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
     }
 };
