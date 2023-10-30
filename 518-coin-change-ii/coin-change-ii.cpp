74aab5dc03b2c6c950538bced6c7a9ceb8debe7c
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-30 21:48:38 +0530

    Time: 23 ms (49.69%) | Memory: 19.1 MB (19.97%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -6,13 +6,14 @@ private:
         if(amount==0) return 1;
 
         if(idx==0){
-            if(amount%coins[0]==0) return 1;
+            if(amount%coins[0]==0) return 1;  //bcz we can take the oth coin any number of time to create the amount required
             else return 0;
         }
 
         if(amount<0) return 0;
 
         int take=0;
+        //bcz we can take same coin any number of times so we don't decrease the index
         if(coins[idx]<=amount) take += solve(amount-coins[idx],coins,n,idx,dp);
         int not_take = solve(amount,coins,n,idx-1,dp);
 
@@ -23,23 +24,22 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,0));
-
-        for(int i=0; i<n; i++){
-            dp[i][0]=1;
-        }
-        for(int t=0; t<=amount; t++) dp[0][t]= (t%coins[0]==0);
+        vector<int>prev(amount+1,0),curr(amount+1,0);
+        
+        for(int t=0; t<=amount; t++) prev[t]= (t%coins[0]==0);
 
         for(int i=1; i<n; i++){
             for(int t=0;t<=amount; t++){
                 int take=0;
-                if(coins[i]<=t) take += dp[i][t-coins[i]];
-                int not_take = dp[i-1][t];
+                if(coins[i]<=t) take += curr[t-coins[i]];
+                int not_take = prev[t];
 
-                dp[i][t] = take + not_take;
+                curr[t] = take + not_take;
             }
+            prev=curr;
         }
 
-        return dp[n-1][amount];
+        return prev[amount];
 
 
     }
