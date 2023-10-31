d838b4dbbc1d5a0b748c2bd630bd2929656e000d
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-31 22:53:21 +0530

    Time: 50 ms (58.66%) | Memory: 24.9 MB (29.73%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,7 +7,7 @@ private:
         if(dp[i][j] != -1) return dp[i][j];
 
         //if the character at the ith index in string1 match with the character at the jth index in string2
-        if(s1[i] == s2[j]) return 1 + LCS(i-1,j-1,s1,s2,dp);
+        if(s1[i] == s2[j]) return dp[i][j] = 1 + LCS(i-1,j-1,s1,s2,dp);
 
         //if they don't match then either the s1[i-1] will match with s2[j] OR s1[i] will match with s2[j-1]. we need to take max from both.
         return dp[i][j] = max(LCS(i-1,j,s1,s2,dp) , LCS(i,j-1,s1,s2,dp));
