1937d7e32a30cb2ed63969690fa4d932e3658529
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-10-31 23:07:06 +0530

    Time: 51 ms (54.20%) | Memory: 24.7 MB (56.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,3 +1,4 @@
+// we do index shifting to one right to perform tabulation. So, the n means n-1 , 1 means 0, and 0 means -1.
 class Solution {
 private:
     int LCS(int i, int j,string&s1, string &s2, vector<vector<int>>&dp){
@@ -17,8 +18,25 @@ public:
         int n = text1.length();
         int m = text2.length();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        return LCS(n-1,m-1,text1,text2,dp);
+        for(int i=0; i<=n; i++){
+           dp[i][0]=0;
+        }
+        for(int j=0; j<=m; j++){
+            dp[0][j]=0;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
+
     }
 };
