6b4e871cd5aa897fe260c962b0ebacb571b843b9
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-11-04 15:27:40 +0530

    Time: 91 ms (30.76%) | Memory: 7.1 MB (93.14%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -31,27 +31,27 @@ public:
        int n = s.length();
        int m = p.length();
 
-       vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+       vector<bool>prev(m+1,false) , curr(m+1,false);
        if(s.length()>0 && p.length()==0) return false;
        if (allstars(m-1,p)) return true;
 
-       dp[0][0]=true;
-       for(int i=1; i<=n; i++) dp[i][0]=false;
-       for(int j=1; j<=m; j++) dp[0][j] = allstars(j-1,p);
+       prev[0]=true;
+       for(int j=1; j<=m; j++) prev[j] = allstars(j-1,p);
 
        for(int i=1; i<=n; i++){
            for(int j=1; j<=m; j++){
                if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                   dp[i][j] = dp[i-1][j-1];
+                   curr[j] = prev[j-1];
                }
                else{
-                   if(p[j-1]=='*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                   else dp[i][j] = false;
+                   if(p[j-1]=='*') curr[j] = prev[j] || curr[j-1];
+                   else curr[j] = false;
                }
            }
+           prev = curr;
        }
 
-       return dp[n][m];
+       return prev[m];
        
     }
 };
