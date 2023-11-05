bfabe1d5bd01896ba129b4f94bfee1c1da44cea3
Mohammad Adil <121096679+m-adil172000@users.noreply.github.com>
2023-11-05 10:14:53 +0530

    Time: 130 ms (75.64%) | Memory: 75.5 MB (92.91%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,7 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int n =  prices.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        //vector<vector<int>>dp(n+1,vector<int>(5,0));
 
         /*
         B S B S
@@ -12,20 +12,22 @@ public:
         we can see that buy is an even number operation and sell is odd number operation
         */
         // we need not to write the base cases bcz they are already 0
+        vector<int>after(5,0),curr(5,0);
 
         for(int i=n-1; i>=0; i--){
             for(int t=0; t<4; t++){
                 if(t%2==0){
                     // Buy
-                    dp[i][t] = max(-prices[i]+dp[i+1][t+1], dp[i+1][t]);
+                    curr[t] = max(-prices[i]+after[t+1], after[t]);
                 }
                 else{
                     //sell
-                    dp[i][t] = max(prices[i] + dp[i+1][t+1],dp[i+1][t]);
+                    curr[t] = max(prices[i] + after[t+1],after[t]);
                 }
             }
+            after = curr;
         }
 
-        return dp[0][0];
+        return after[0];
     }
 };
